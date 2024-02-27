7a5a1863828ce2a0b65410615fafab0369ba8f5d
JTBarsre <jtb@aveo.dk>
2024-02-27 13:05:33 +0100

    1.0.0

---
+++ test.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * Plugin Name:       Test
+ * Plugin URI:        https://aveo.dk/
+ * Description:       Tester funktionalitet for Aveo
+ * Version:           1.0.0
+ * Author:            Aveo
+ * Update URI:        https://aveo.dk/
+ * Text Domain:       test
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; 
+}

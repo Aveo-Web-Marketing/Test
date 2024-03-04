517ef08bbec5e6a83a14a64eb97584b9fe46fd87
JTBarsre <jtb@aveo.dk>
2024-03-04 14:53:53 +0100

    1.0.8

--- test.php
+++ test.php
@@ -3,7 +3,7 @@
  * Plugin Name:       Test
  * Plugin URI:        https://aveo.dk/
  * Description:       Tester funktionalitet for Aveo
- * Version:           1.0.0
+ * Version:           1.0.8
  * Author:            Aveo
  * Update URI:        https://aveo.dk/
  * Text Domain:       test
